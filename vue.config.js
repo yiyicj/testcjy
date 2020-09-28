87568134677081e20897b2f0eadaa27af3518117
caojuan <384758097qq.com>
2020-09-28 23:47:52 +0800

    修改

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  publicPath: '/cj/',
+  productionSourceMap: false,
+  devServer: {
+    // open: true,
+    port: 8000,
+    disableHostCheck: true,
+   
+  },
+  chainWebpack: config => {
+    config.resolve.symlinks(true)
+  },
+  configureWebpack: config => {
+    if (process.env.VUE_APP_MODE === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.warnings = false
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true
+      config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = ['console.log']
+    }
+    config.externals = {
+      AMap: 'AMap',
+    }
+  }
+}
